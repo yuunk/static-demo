d4cf7306c59a44972fe1f82a30208bc3502a7812
dchip0 <dchipself0@gmail.com>
2020-10-13 00:24:59 +0900

    add: opacity slider

---
+++ demo/index.js
@@ -0,0 +1,91 @@
+const next = document.getElementById('slide_next');
+const current = document.getElementById('slide_current');
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
+
+class OpacitySlider {
+
+    constructor(next, current, images) {
+        this.next = next;
+        this.current = current;
+        this.images = images;
+        this.counter = 1;
+        this.currentIndex = 0;
+        this.nextIndex = 1;
+        this.initSlideImage();
+    }
+
+    initSlideImage() {
+        current.style.backgroundImage = 'url(' + images[0] + ')';
+        next.style.backgroundImage = 'url(' + images[1] + ')';
+    }
+
+    startSlider() {
+
+
+        setInterval(() => {
+
+            let promise = new Promise((resolve, reject) => {
+
+                let opacityTransition = setInterval(() => {
+
+                    const opacity = 1;
+
+                    this.counter++;
+                    this.current.style.opacity = opacity - this.counter * 0.01;
+
+                    if (this.counter === 100) {
+                        this.resetOpacity();
+                        clearInterval(opacityTransition);
+                        resolve();
+                    }
+
+                }, 10);
+
+            });
+
+            promise.then(() => {
+                console.log('ok');
+
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        this.changeBothImage();
+                        this.updateIndex();
+                        this.current.style.opacity = 1;
+                        resolve();
+                    }, 500);
+                });
+
+            }).then(() => {
+                this.changeImage();
+            }).catch(() => {
+                console.log('fail');
+            });
+
+        }, 4000);
+
+    }
+
+    resetOpacity() {
+        this.counter = 1;
+    }
+
+    changeBothImage() {
+        current.style.backgroundImage = 'url(' + images[this.nextIndex] + ')';
+        next.style.backgroundImage = 'url(' + images[this.nextIndex] + ')';
+    }
+    updateIndex() {
+        this.currentIndex = this.nextIndex;
+        if (this.nextIndex === this.images.length - 1) {
+            this.nextIndex = 0;
+        } else {
+            this.nextIndex++;
+        }
+    }
+    changeImage() {
+        next.style.backgroundImage = 'url(' + images[this.nextIndex] + ')';
+    }
+
+}
+
+const slider = new OpacitySlider(next, current, images);
+slider.startSlider();
